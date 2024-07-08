9cf287e127cee438af686000e40ef6c5f44de586
danromlop <125254939+danromlop@users.noreply.github.com>
2024-07-08 18:37:56 +0200

    Update datos_conexion.php

--- pac/modelo/datos_conexion.php
+++ pac/modelo/datos_conexion.php
@@ -1,8 +1,8 @@
 <?php
 define("DATABASE","thenews");
-define("HOST", "localhost"); //3306
+define("HOST", "localhost"); 
 define("USER", "root");
-define("PASSWORD", "cabeson123");
+define("PASSWORD", "password");
 ?>
 
 
